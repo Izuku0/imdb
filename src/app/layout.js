7dcae93336febebfcecea61c16fc433d3ea99960
Itachi <donkailash786@gmail.com>
2023-03-20 22:13:11 +0530

    create the header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import Header from '@/components/Header'
 import './globals.css'
+
 
 export const metadata = {
   title: 'IMDb',
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header/>
+        
+        {children}</body>
     </html>
   )
 }
