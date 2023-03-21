34a89767439fb251e721ee336e7dcc846dcd88d5
Itachi <donkailash786@gmail.com>
2023-03-21 19:24:08 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
+       <p>
+       The Internet Movie Database (IMDb) is an online database containing information and statistics about movies, TV shows and video games as well as actors, directors and other film industry professionals. This information can include lists of cast and crew members, movie release dates and box office information, plot summaries, trailers, actor and director biographies and other trivia.
+       </p>
+       <p>
+       Information on IMDb comes from a variety of sources, such as filmmakers, film studios, on-screen credits and other official sources. However, much of the information comes from IMDb users themselves, who can submit facts in a wiki-style format. Unlike traditional wiki sites, IMDb always authenticates information before it appears online -- although errors do show up, and the website allows users to report possible mistakes so they can be fixed.
+       </p>
+       <p>
+       Users can also submit reviews of movies and TV shows on a one to ten scale, which are then used to create a weighted mean of all user reviews to be displayed on the movie or TV show's page. Also, these ratings are used to compile IMDb's Top Rated and Lowest Rated lists of movies and TV shows.
+
+IMDb was first published in 1990 by Col Needham, a computer programmer, as a group of scripts which allowed users to search a list of film credits that were compiled with the help of a USENET group. The database was incorporated in 1996. In 1998, Internet Movie Database Ltd. became a subsidiary of Amazon, which uses it as a way to advertise movies and DVDs which it sells on its main website.
+       </p>
+       
+        </div>
+  )
+}
