11f965da7c850ca3011a6693aa9af5765ad511ce
Itachi <donkailash786@gmail.com>
2023-03-22 16:23:52 +0530

    final1

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div className='max-w-6xl mx-auto space-y-4 p-4'>
-        <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
-       <p>
-       The Internet Movie Database (IMDb) is an online database containing information and statistics about movies, TV shows and video games as well as actors, directors and other film industry professionals. This information can include lists of cast and crew members, movie release dates and box office information, plot summaries, trailers, actor and director biographies and other trivia.
-       </p>
-       <p>
-       Information on IMDb comes from a variety of sources, such as filmmakers, film studios, on-screen credits and other official sources. However, much of the information comes from IMDb users themselves, who can submit facts in a wiki-style format. Unlike traditional wiki sites, IMDb always authenticates information before it appears online -- although errors do show up, and the website allows users to report possible mistakes so they can be fixed.
-       </p>
-       <p>
-       Users can also submit reviews of movies and TV shows on a one to ten scale, which are then used to create a weighted mean of all user reviews to be displayed on the movie or TV show's page. Also, these ratings are used to compile IMDb's Top Rated and Lowest Rated lists of movies and TV shows.
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs.
+      </p>
 
-IMDb was first published in 1990 by Col Needham, a computer programmer, as a group of scripts which allowed users to search a list of film credits that were compiled with the help of a USENET group. The database was incorporated in 1996. In 1998, Internet Movie Database Ltd. became a subsidiary of Amazon, which uses it as a way to advertise movies and DVDs which it sells on its main website.
-       </p>
-       
-        </div>
-  )
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        movies from all around the world, along with the latest news, reviews,
+        and trailers. Our movie database is constantly updated with new
+        releases, ensuring that you have access to the latest and greatest in
+        the world of cinema. You can search for movies by title, director,
+        actor, genre, or release date, making it easy to find the perfect movie
+        for any occasion.
+      </p>
+
+      <p>
+        In addition to our extensive movie database, we also offer a platform
+        for movie lovers to connect and share their thoughts on the latest
+        releases. Our community section includes a forum where you can discuss
+        your favorite films with like-minded individuals and read reviews and
+        ratings from other users. We also have a section dedicated to movie news
+        and trailers, keeping you updated with the latest happenings in the
+        world of cinema. Thank you for visiting our website and we hope you
+        enjoy your time browsing through our movie database. If you have any
+        feedback or suggestions, please feel free to contact us. We are always
+        looking for ways to improve and enhance the user experience on our
+        website. Happy browsing!
+      </p>
+    </div>
+  );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,31 +1,37 @@
-import Loading from '@/app/loading'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import {AiFillHeart} from 'react-icons/ai'
+import Image from "next/image";
+import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
 
-const Card = ({result}) => {
+export default function Card({ result }) {
   return (
-    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 group sm:m-2 transition-shadow duration-200'>
-
-        <Link href={`/movie/${result.id}`}>
-            <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={300} alt="movie image"
-             className='sm:rounded-t-lg group-hover:opacity-80 transition duration-200' 
-             style={{maxWidth:"100%", height:"auto"}}
-             placeholder="blur"
-            blurDataURL="/spinner.svg"
-             ></Image>
-             <div className='p-2'>
-                <p>{result.overview.substring(0,40)}...</p>
-              <h2 className='text-lg font-bold'>
-                {result.title || result.name}
-              </h2>
-              <p className='flex items-center'>{result.first_air_date || result.release_date}
-             <AiFillHeart className='text-red-500 ml-3 mr-1'/>{result.vote_count}</p>
-             </div>
-        </Link>
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          alt="image is not available"
+        ></Image>
+        <div className="p-2">
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <h2 className="truncate text-lg font-bold">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
+          </p>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
-
-export default Card
